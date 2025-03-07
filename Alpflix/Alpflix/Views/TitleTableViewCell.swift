7066ce8fa3af666984956adb86b6f669b7c491ec
Alperen Çerkez <alperencerkezz@gmail.com>
2025-03-07 12:53:03 +0300

    Create TitleTableViewCell.swift

---
+++ Alpflix/Alpflix/Views/TitleTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  TitleTableViewCell.swift
+//  Alpflix
+//
+//  Created by Alperen Çerkez on 7.03.2025.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    static let identifier = "TitleTableViewCell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlesPosterUIImageView: UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlesPosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let titlesPosterUIImageViewConstraints = [
+            titlesPosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlesPosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlesPosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlesPosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string: model.posterURL) else {
+            return
+        }
+        titlesPosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
